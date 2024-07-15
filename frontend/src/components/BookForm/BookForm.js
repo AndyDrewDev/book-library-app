c21f34c5fa138102e44a8b3b24c68682eedd0ad2
AndyDrewDev <andydrew.stamp@gmail.com>
2024-07-15 19:36:39 +0300

    Added BookForm, BookList and Filter components

---
+++ frontend/src/components/BookForm/BookForm.js
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import './BookForm.css'
+
+const BookForm = () => {
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    if (title && author) {
+      setTitle('')
+      setAuthor('')
+    }
+  }
+  return (
+    <div className='app-block book-form'>
+      <h2>Add a New Book </h2>
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor='title'>
+            Title:
+            <input
+              type='text'
+              id='title'
+              value={title}
+              onChange={(event) => setTitle(event.target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label htmlFor='author'>
+            Author:
+            <input
+              type='text'
+              id='author'
+              value={author}
+              onChange={(event) => setAuthor(event.target.value)}
+            />
+          </label>
+        </div>
+        <button type='submit'>Add Book</button>
+      </form>
+    </div>
+  )
+}
+
+export default BookForm
